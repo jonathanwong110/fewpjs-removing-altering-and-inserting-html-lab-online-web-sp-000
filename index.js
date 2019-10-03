07ee96ee1cbbc72ffe773381320727f757fe62a9
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 05:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  // Write your code here!
  
 document.body.remove("main#main")
-let newHeader = document.getElementById("div#header");
-header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+let newHeader = document.getElementById("div#victory");
+newHeader.innerHTML = "<h1>victory</h1>";
