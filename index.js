de7a25c30b3b30afb2d6473d4d6d62c8f981c64d
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 04:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
  // Write your code here!
+ 
+ <!DOCTYPE html>
+<html>
+  <head>
+    <title>Creating and Inserting DOM Elements</title>
+  </head>
+  <script type="application/javascript">
+  </script>
+  <body>
+    <main id="main"></main>
+  </body>
+</html>
