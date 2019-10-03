b9a89165c046b445811ceb21f791600c093feeb3
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 05:42:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  // Write your code here!
  
 document.body.remove("main#main")
-let newHeader = document.getElementById("div#victory");
+let newHeader = document.getElementById("h1#victory");
 newHeader = "<h1>victory</h1>";
