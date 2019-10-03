a5073346df582ec4f5ae36bf0cba453c79670e19
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 06:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 document.body.remove("main#main");
 let newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerText = "is the champion"
+newHeader.innerText = "First Place is the champion";
