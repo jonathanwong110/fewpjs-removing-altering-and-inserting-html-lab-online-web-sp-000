8101b53116b0031bf5f4f344b178a3cd8203732c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 05:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  // Write your code here!
  
 document.body.remove("main#main")
-let newHeader = document.querySelector("id#victory")
+let newHeader = document.querySelector("H1#victory")
 newHeader = "H1"
