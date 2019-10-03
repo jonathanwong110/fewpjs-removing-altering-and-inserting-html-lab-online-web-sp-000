c719a81b6da8f1d9aac5484f4039536bd2eddc6c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 05:50:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  // Write your code here!
  
-document.body.remove("main#main")
-let newHeader = document.createElement("h1")
+document.body.remove("main#main");
+let newHeader = document.createElement("h1");
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "victory";
